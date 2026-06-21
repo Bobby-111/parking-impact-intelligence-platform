6ad0dbd79741e0cd9d7515e7ac413a93f3aa29e1
BHARATH CHILAKA <johndavidch62461@gmail.com>
2026-06-21 21:18:56 +0530

    Update index.ts

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://parking-impact-intelligence-platform.onrender.com/api';
 
 export const fetchKPIs = async () => {
   const res = await axios.get(`${API_URL}/kpis`);
